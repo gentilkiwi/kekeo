4d0451038150b1c167c01e8eb69c8c48eec3fab2
Benjamin DELPY <benjamin@gentilkiwi.com>
2015-03-16 23:17:54 +0100

    Licence update in c/h files

--- ms14068/ms14068.c
+++ ms14068/ms14068.c
@@ -1,7 +1,7 @@
 /*	Benjamin DELPY `gentilkiwi`
 	http://blog.gentilkiwi.com
 	benjamin@gentilkiwi.com
-	Licence : http://creativecommons.org/licenses/by/3.0/fr/
+	Licence : https://creativecommons.org/licenses/by-nc-sa/4.0/
 */
 #include "ms14068.h"
 
